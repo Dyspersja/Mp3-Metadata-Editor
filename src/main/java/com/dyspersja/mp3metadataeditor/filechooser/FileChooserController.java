b223ff5470bdd38b4ce40ac42e12e8ee0113b3c1
Dyspersja <dyspersjamusic@gmail.com>
2024-04-13 18:40:02 +0200

    Added file chooser for mp3 files

--- src/main/java/com/dyspersja/mp3metadataeditor/filechooser/FileChooserController.java
+++ src/main/java/com/dyspersja/mp3metadataeditor/filechooser/FileChooserController.java
@@ -3,8 +3,12 @@ package com.dyspersja.mp3metadataeditor.filechooser;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.stage.FileChooser;
+import javafx.stage.FileChooser.ExtensionFilter;
 import javafx.stage.Stage;
 import lombok.Setter;
+
+import java.io.File;
 
 public class FileChooserController {
 
@@ -16,6 +20,20 @@ public class FileChooserController {
     private Stage window;
 
     public void choseFile(ActionEvent event) {
+        File chosenFile = openFileChooser();
+        if (chosenFile != null) {
+            openEditorWindow(chosenFile);
+        }
+    }
+
+    private File openFileChooser() {
+        FileChooser fileChooser = new FileChooser();
+        ExtensionFilter extensionFilter = new ExtensionFilter("MP3 files (*.mp3)", "*.mp3");
+        fileChooser.getExtensionFilters().add(extensionFilter);
+        return fileChooser.showOpenDialog(window);
+    }
+
+    private void openEditorWindow(File chosenFile) {
 
     }
 }
