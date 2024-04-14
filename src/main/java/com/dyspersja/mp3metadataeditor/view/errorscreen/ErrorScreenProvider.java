edc365bfabbd39b4ef99d1ffefcf26a259029efa
Dyspersja <dyspersjamusic@gmail.com>
2024-04-14 12:28:14 +0200

    Added second error screen for file reading

--- src/main/java/com/dyspersja/mp3metadataeditor/view/errorscreen/ErrorScreenProvider.java
+++ src/main/java/com/dyspersja/mp3metadataeditor/view/errorscreen/ErrorScreenProvider.java
@@ -4,12 +4,27 @@ import javafx.application.Platform;
 import javafx.scene.control.Alert;
 
 public class ErrorScreenProvider {
-    public static void displayErrorWindow(Exception e) {
+
+    public static void displayFXMLErrorWindow(Exception e) {
+        String headerText = "Error Loading FXML";
+        String contentText = "An error occurred while loading the FXML file: " + e.getMessage();
+
+        displayErrorWindow(headerText, contentText);
+    }
+
+    public static void displayFileErrorWindow(Exception e) {
+        String headerText = "Error Reading File";
+        String contentText = "An error occurred while reading the file: " + e.getMessage();
+
+        displayErrorWindow(headerText, contentText);
+    }
+
+    private static void displayErrorWindow(String headerText, String contentText) {
         Alert alert = new Alert(Alert.AlertType.ERROR);
 
         alert.setTitle("Error");
-        alert.setHeaderText("Error loading FXML");
-        alert.setContentText("An error occurred while loading the FXML file: " + e.getMessage());
+        alert.setHeaderText(headerText);
+        alert.setContentText(contentText);
 
         alert.showAndWait();
         Platform.exit();
